9d2b403d239bd52fca1b03685a4788f18c03da2d
Felix Wittwer <120730441+felixwittwer@users.noreply.github.com>
2022-12-30 09:46:59 +0100

    Initial commit

---
+++ InputLib.c
@@ -0,0 +1,50 @@
+/*************************************************************/
+/** InputLib - input library for fx-9860GII and GIII        **/
+/** InputLib is free software to use                        **/
+/**                                                         **/
+/** @author Felix Wittwer                                   **/
+/**                                                         **/
+/** @file InputLib.h                                        **/
+/** Include header for MonochromeLib                        **/
+/**                                                         **/
+/** @date 12-29-2022                                        **/
+/*************************************************************/
+
+#include "InputLib.h"
+#include "fxlib.h"
+#include "stdlib.h"
+
+/******************************/
+/** Dependencies management  **/
+/******************************/
+
+#ifdef IL_ALL
+    #define IL_TEST
+    #define IL_SINGLE_SELECTION
+    #define IL_MULTI_SELECTION
+#endif
+
+/***************/
+/** Functions **/
+/***************/
+
+#ifdef IL_TEST
+void IL_Test()
+{
+    PrintXY(1,1,(unsigned char*)"Test",0);
+}
+#endif
+
+#ifdef IL_SINGLE_SELECTION
+void IL_Single_Selection(int x, int y, unsigned char content)
+{
+    PrintXY(x, y, content, 0);
+}
+#endif
+
+#ifdef IL_MULTI_SELECTION
+void IL_Multi_Selection(int x, int y, unsigned char content)
+{
+    PrintXY(x, y, content, 0);
+}
+#endif
---
+++ InputLib.h
@@ -0,0 +1,44 @@
+/*************************************************************/
+/** InputLib - input library for fx-9860GII and GIII        **/
+/** InputLib is free software to use                        **/
+/**                                                         **/
+/** @author Felix Wittwer                                   **/
+/**                                                         **/
+/** @file InputLib.h                                        **/
+/** Include header for MonochromeLib                        **/
+/**                                                         **/
+/** @date 12-29-2022                                        **/
+/*************************************************************/
+
+#ifdef INPUTLIB
+#define INPUTLIB
+
+/****************************************************/
+/** uncomment if you want to use the functions     **/
+/****************************************************/
+
+#define IL_ALL //defines all functions
+#define IL_TEST
+
+#define IL_SINGLE_SELECTION
+#define IL_MULTI_SELECTION
+
+
+/**************************/
+/** Functions prototypes **/
+/**************************/
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+void IL_Test();
+void IL_Single_Selection(int x, int y, unsigned char *content);
+void IL_Multi_Selection(int x, int y, unsigned char *content);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //INPUTLIB
